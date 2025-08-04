5bc75055a6f3d02af7cbc387996b23c7c8d2d484
ChapCunningham <chapcunningham@gmail.com>
2025-08-04 16:18:50 -0400

    Update ccbl_pitchers_2025.py

--- ccbl_pitchers_2025.py
+++ ccbl_pitchers_2025.py
@@ -676,7 +676,7 @@ def plot_pitch_movement(pitcher_name, batter_side, strikes, balls, date_filter_o
                     line=dict(width=1, color="white")  # White edge for better contrast
                 ),
                 text=pitch_data.apply(
-                    lambda row: f"Date: {row['Date']}<br>RelSpeed: {row['RelSpeed']}<br>iVB: {row['InducedVertBreak']}<br>HB: {row['HorzBreak']}<br>Pitch#: {row['PitchNo']}",
+                    lambda row: f"Date: {row['Date']}<br>RelSpeed: {row['RelSpeed']}<br>iVB: {row['InducedVertBreak']}<br>HB: {row['HorzBreak']}<br>Spin: {row['SpinRate']}<br>Pitch#: {row['PitchNo']}",
                     axis=1
                 ),
                 hoverinfo='text'
